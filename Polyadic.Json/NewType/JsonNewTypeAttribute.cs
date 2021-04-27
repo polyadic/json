319ac9c120fadb6199f9365247fbb583f1b1e198
Ruben Schmidmeister <ruben.schmidmeister@messerli.ch>
2021-04-27 12:02:55 +0200

    Remove restriction of attribute usage

--- Polyadic.Json/NewType/JsonNewTypeAttribute.cs
+++ Polyadic.Json/NewType/JsonNewTypeAttribute.cs
@@ -3,7 +3,6 @@ using System.Text.Json.Serialization;
 
 namespace Polyadic.Json.NewType
 {
-    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct)]
     public sealed class JsonNewTypeAttribute : JsonConverterAttribute
     {
         public override JsonConverter CreateConverter(Type typeToConvert) => new JsonNewTypeJsonConverterFactory();
